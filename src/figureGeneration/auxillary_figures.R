65bb064e49a1ab38531d5979ca0a75e02e806d5a
tore.brede@gmail.com <tore.brede@gmail.com>
2016-03-14 15:38:59 +0100

    Adding code for generating some plots showing how use of sampling can be used to approximate a distribution.

--- src/figureGeneration/auxillary_figures.R
+++ src/figureGeneration/auxillary_figures.R
@@ -1,3 +1,4 @@
+library(MASS)
 library(latex2exp)
 
 # Show example plots for illustrating importance sampling
@@ -9,3 +10,30 @@ curve(VectorizedNormal, add = T, col = "blue")
 
 text = c(latex2exp("p(x)"), latex2exp("g(x)"))
 legend("topright", legend = text, lty = c(1, 1), lwd = c(2.5, 2.5), col = c("red", "blue"))
+
+# Show approximations to normal distribution
+par(mfrow = c(1, 3))
+
+sample.set1 <- rnorm(10)
+truehist(sample.set1, ann = F)
+VectorizedNormal2 <- Vectorize(function(x) { dnorm(x)})
+curve(VectorizedNormal2, col = "blue", xlim = c(-3, 3), ann = F, add = T)
+title(sub = "10 samples")
+
+sample.set1 <- rnorm(100)
+truehist(sample.set1, ann = F)
+VectorizedNormal2 <- Vectorize(function(x) { dnorm(x)})
+curve(VectorizedNormal2, col = "blue", xlim = c(-3, 3), ann = F, add = T)
+title(sub = "100 samples")
+
+sample.set1 <- rnorm(10000)
+truehist(sample.set1, ann = F)
+VectorizedNormal2 <- Vectorize(function(x) { dnorm(x)})
+curve(VectorizedNormal2, col = "blue", xlim = c(-3, 3), ann = F, add = T)
+title(sub = "10000 samples")
+
+par(mfrow = c(1, 1))
+
+
+
+
