4e2f3d0cb73b5e5899a49836a238173494cd5410
tore.brede@gmail.com <tore.brede@gmail.com>
2016-05-16 12:17:24 +0200

    Adding plot illustrating conjugate distributions.

--- src/figureGeneration/conjugate_distributions.R
+++ src/figureGeneration/conjugate_distributions.R
@@ -1,13 +1,11 @@
 library(latex2exp)
 
-
 n <- 10
 p <- 0.3
 y <- 3
 
 alpha <- 2
 beta <- 4
-
 
 VectorizedPrior <- Vectorize(function(theta) { dbeta(theta, alpha, beta) })
 curve(VectorizedPrior, col = "blue", ann = F)
@@ -21,46 +19,51 @@ VectorizedPosterior <- Vectorize(function(theta) { dbeta(theta, alpha + y, beta 
 curve(VectorizedPosterior, col = "blue", ann = F)
 title(main = latex2exp("$cp(\\theta |y)$"), xlab = latex2exp("$\\theta$"))
 
-x.values <- seq(0, 1, 0.05)
-plot(c(0, 1), c(0, 3), type = "n")
+
+png("conjugate_distributions.png", width = 720, height = 240)
+par(mfrow = c(1, 3))
+
+# Setup empty plot
+x.values <- seq(0, 1, 0.0025)
+plot(c(0, 1), c(0, 3), type= "n", ann = F)
+title(xlab = latex2exp("$\\theta$"))
 
 prior.color <- rgb(0, 1, 0, 0.5)
 y.values2 <- sapply(x.values, VectorizedPrior)
 polygon(c(x.values, rev(x.values)), c(y.values2, rep(0, length(y.values2))), col = prior.color)
 
-likelihood.color <- rgb(0, 0, 1, 0.5)
+likelihood.color <- rgb(0, 0, 1, 3.5)
 y.values3 <- sapply(x.values, VectorizedLikelihood)
 polygon(c(x.values, rev(x.values)), c(y.values3, rep(0, length(y.values3))), col = likelihood.color)
 
+text = c(latex2exp("$p(x|\\theta)$"), latex2exp("$p(\\theta)$"))
+legend("topright", legend = text, lty = c(1, 1), lwd = c(2.5, 2.5), col = c(likelihood.color, prior.color))
 
-plot(c(0, 1), c(0, 3), type = "n")
+
+# The normalization constant is 0.1598402
+
+plot(c(0, 1), c(0, 3.5), type = "n", ann = F)
+y.values.test <- sapply(x.values, function(x) {(1 / 0.1598402) * VectorizedPrior(x) *  VectorizedLikelihood(x)})
+posterior.color <- rgb(1, 0, 0)
+polygon(c(x.values, rev(x.values)), c(y.values.test, rep(0, length(y.values.test))), col = rgb(0, 0, 1))
+title(xlab = latex2exp("$\\theta$"))
+
+text = latex2exp("$\\frac{p(x|\\theta)\\times p(\\theta)}{0.1598402}")
+legend("topright", legend = text, lty = c(1, 1), lwd = c(2.5, 2.5), col = rgb(0, 0, 1), adj = c(0, 0.3))
+
+
+plot(c(0, 1), c(0, 3.5), type = "n", ann = F)
 y.values <- sapply(x.values, VectorizedPosterior)
-posterior.color <- rgb(1, 0, 0)
+posterior.color <- rgb(1, 1, 0)
 polygon(c(x.values, rev(x.values)), c(y.values, rep(0, length(y.values))), col = posterior.color)
+title(xlab = latex2exp("$\\theta$"))
 
+text = latex2exp("$p(\\theta | x)$")
+legend("topright", legend = text, lty = c(1, 1), lwd = c(2.5, 2.5), col = posterior.color)
 
-
-plot(c(0, 1), c(0, 3), type = "n")
-y.values.test <- sapply(x.values, function(x) {VectorizedPrior(x) *  VectorizedLikelihood(x)})
-posterior.color <- rgb(1, 0, 0)
-polygon(c(x.values, rev(x.values)), c(y.values.test, rep(0, length(y.values.test))), col = posterior.color)
-
+dev.off()
 
 plot(c(0, 1), c(0, 3), type = "n")
 y.values.test2 <- sapply(x.values, function(x) { (gamma(n + alpha + beta) / (gamma(y + alpha) * gamma(n - y + beta))) * x^(y + alpha - 1) * (1 - x)^(n - y + beta - 1) })
 posterior.color <- rgb(1, 0, 0)
-polygon(c(x.values, rev(x.values)), c(y.values.test2, rep(0, length(y.values.test2))), col = posterior.color)
-
-
-
-# conditionalStdDev = sqrt(diag(sigma))
-# lowerBorder = mu - 2 * conditionalStdDev
-# upperBorder = mu + 2 * conditionalStdDev
-# polygonUpper = cbind(1:47, upperBorder)
-# polygonLower = cbind(47:1, rev(lowerBorder))
-# polygon(c(polygonUpper, polygonLower))
-# vertices = rbind(polygonUpper, polygonLower)
-# plot(mu, ylim = c(min(vertices[, 2]), max(vertices[, 2])), type = "l")
-# polygon(vertices[, 1], vertices[, 2], col = "grey90", border = NA)
-# lines(mu)
-
+polygon(c(x.values, rev(x.values)), c(y.values.test2, rep(0, length(y.values.test2))), col = rgb(0, 0, 1))
